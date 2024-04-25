08b6a09b1e087fdbf8e17d217ff7bd32836e5060
Sleiman Safaoui <snsafaoui@gmail.com>
2024-04-24 19:42:26 -0500

    updating license notice in some scripts

--- cvxRiskOpt/wass_risk_opt_pb.py
+++ cvxRiskOpt/wass_risk_opt_pb.py
@@ -1,14 +1,25 @@
 """
-Copyright 2024 Sleiman Safaoui
-Licensed under the GNU GENERAL PUBLIC LICENSE Version 3.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    https://www.gnu.org/licenses/gpl-3.0.en.html
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+cvxRiskOpt: Risk-Based Optimization tool using CVXPY and CVXPYgen
+Copyright (C) 2024  Sleiman Safaoui
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+GitHub:
+@The-SS
+Email:
+snsafaoui@gmail.com
+sleiman.safaoui@utdallas.edu
 """
 """
 CVXPY-based data-driven optimization problems that compute risk metrics.
