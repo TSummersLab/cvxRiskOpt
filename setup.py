e2d52fcd2304aafe7d427e9bd1e7660021bcf44c
Sleiman Safaoui <snsafaoui@gmail.com>
2024-04-24 19:18:04 -0500

    incrementing patch number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cvxRiskOpt",
-    version="0.1.0",
+    version="0.1.1",
     description="Risk-Based Optimization tool using CVXPY and CVXPYgen",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
