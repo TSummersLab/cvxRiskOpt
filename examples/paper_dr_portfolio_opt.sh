ecd73557b583ecee7dbc9bccb42e21b3976737a7
Sleiman Safaoui <snsafaoui@gmail.com>
2024-05-06 12:14:14 -0500

    clean up - paper experiments shell scripts

--- examples/paper_dr_portfolio_opt.sh
+++ examples/paper_dr_portfolio_opt.sh
@@ -1,22 +1,19 @@
 
-num_sim=100
 script="paper_dr_portfolio_opt.py"
 
-#N_list=(10 20 50 100 150 200 250 300)
-#N_list=(25 50 75 100 125 150 175 200 225 250 275 300 325)
+N_list=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
+num_sim=100
 
-## CLARABEL
-#N_list=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
-#for N in "${N_list[@]}"; do
-#    echo "Solving with CLARABEL for N = $N samples"
-#  python3 $script --num_samp $N --num_sim $num_sim --gen_code 1 --keep_init_run 0 --solver "CLARABEL"
-#  sleep .5
-#done
+# CLARABEL
+for N in "${N_list[@]}"; do
+    echo "Solving with CLARABEL for N = $N samples"
+  python3 $script --num_samp $N --num_sim $num_sim --gen_code 1 --keep_init_run 0 --solver "CLARABEL"
+  sleep .5
+done
 
 # ECOS
-#N_list=(10 20 50 75 100 125 150 175 200 225 250 275 300 350 400 450 500 600 700 800 900 1000)
-#N_list=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
-N_list=(220 230 250)
+# (for some reason, ECOS produces a wrong result, for this problem, when resolving with C code.
+# This resolves this issue.)
 for N in "${N_list[@]}"; do
   echo "Solving with ECOS for N = $N samples"
   echo "i = 1 / $num_sim"
--- examples/paper_mhe.sh
+++ examples/paper_mhe.sh
@@ -1,10 +1,10 @@
 
 script="paper_mhe.py"
 
+H_list=(10 20 30 40 50 60 70 80 90)
 sim_steps=200
 constraint_type="moment"
 
-H_list=(10 20 30 40 50 60 70 80 90)
 for H in "${H_list[@]}"; do
   echo "Solving with ECOS with H=$H"
   python3 $script --horizon $H --sim_steps $sim_steps --constraint_type $constraint_type --keep_init_run 0 --solver "ECOS"
